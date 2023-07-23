b85547936eb345161ffdcf22f55f2362565d9ceb
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2023-07-22 21:00:26 -0400

    navbar finished , mobile view next

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -6,14 +6,53 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
 
-        <nav className={styles.navbar}>
-            <h1 className={styles.logo}>RO</h1>
+        <nav className={styles.navBar}>
 
-            <div onClick={() => setIsOpen(!isOpen)} className={styles.hamburger}>
-                <div className={isOpen ? `${styles.bar} ${styles.bar1}` : `${styles.bar}`}></div>
-                <div className={isOpen ? `${styles.bar} ${styles.bar2}` : `${styles.bar}`}></div>
-                <div className={isOpen ? `${styles.bar} ${styles.bar3}` : `${styles.bar}`}></div>
-                <div className={isOpen ? `${styles.mobileLinks}` : `${styles.mobileClosed}`} >
+            <div className={styles.logo} >
+                <h1 className={styles.logo}>RO</h1>
+            </div>
+
+            <div className={styles.menu}>
+                <div onClick={() => setIsOpen(!isOpen)} className={styles.hamburger}>
+                    <div className={isOpen ? `${styles.bar} ${styles.bar1}` : `${styles.bar}`}></div>
+                    <div className={isOpen ? `${styles.bar} ${styles.bar2}` : `${styles.bar}`}></div>
+                    <div className={isOpen ? `${styles.bar} ${styles.bar3}` : `${styles.bar}`}></div>
+                    <div className={isOpen ? `${styles.mobileLinks}` : `${styles.mobileClosed}`} >
+
+                    <ul className={styles.navLinks}>
+                            <li>
+                                <Link href="/">Home</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/about">About</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/services">Services</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/contact">Contact</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/work">Work</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/blog">Blog</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/shop">Shop</Link>
+                            </li>
+                    </ul>
+
+                    </div>
+                </div>
+
+                <div className={styles.mainNavigation} >
                     <ul className={styles.navLinks}>
                         <li>
                             <Link href="/">Home</Link>
@@ -32,57 +71,27 @@ const Navbar = () => {
                         </li>
 
                         <li>
-                            <Link href="/work">Work</Link>
-                        </li>
+                            <p className={styles.more}>More</p>
+                            <ul className={styles.dropdown}>
+                                <li>
+                                    <Link href="/work">Work</Link>
+                                </li>
 
-                        <li>
-                            <Link href="/blog">Blog</Link>
-                        </li>
+                                <li>
+                                    <Link href="/blog">Blog</Link>
+                                </li>
 
-                        <li>
-                            <Link href="/shop">Shop</Link>
+                                <li>
+                                    <Link href="/shop">Shop</Link>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
 
                 </div>
-            </div>
-            <div className={styles.mainNavigation} >
-                <ul className={styles.navLinks}>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-
-                    <li>
-                        <Link href="/about">About</Link>
-                    </li>
-
-                    <li>
-                        <Link href="/services">Services</Link>
-                    </li>
-
-                    <li>
-                        <Link href="/contact">Contact</Link>
-                    </li>
-
-                    <li>
-                        <p className={styles.more}>More</p>
-                        <ul className={styles.dropdown}>
-                            <li>
-                                <Link href="/work">Work</Link>
-                            </li>
-
-                            <li>
-                                <Link href="/blog">Blog</Link>
-                            </li>
-
-                            <li>
-                                <Link href="/shop">Shop</Link>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
 
             </div>
+
 
         </nav>
     );
