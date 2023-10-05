69a2ea3c0a3d889f19204141ce28292f8e44e669
Ramzi Osta <ramziosta@mac-mini.myfiosgateway.com>
2023-10-05 13:20:04 -0400

    where section started

--- components/where/index.jsx
+++ components/where/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./who.module.scss";
+import styles from "./where.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUserLarge,
@@ -18,75 +18,16 @@ function Where() {
   return (
     <>
       <div className={styles.container}>
-        {/* about me header */}
-        <div className={styles.aboutHeader}>
-          {/* left section */}
-          <div className={styles.leftSection}>
-            <div className={styles.info}>
-              <div className={styles.who}>
-                <FontAwesomeIcon icon={faUserLarge} className={styles.fa} />
-                <span className={styles.w}>W</span>ho
-              </div>
-
-              <div className={styles.what}>
-                <FontAwesomeIcon icon={faCircleInfo} className={styles.fa} />
-                <span className={styles.w}>W</span>hat
-              </div>
-
-              <div className={styles.where}>
-                <FontAwesomeIcon icon={faLocationDot} className={styles.fa} />
-                <span className={styles.w}>W</span> here & Ho
-                <span className={styles.w}>w</span>
-              </div>
-              <div className={styles.when}>
-                <FontAwesomeIcon icon={faCalendarDays} className={styles.fa} />
-                <span className={styles.w}>W</span>hen
-              </div>
-            </div>
-          </div>
-
-          {/* right section */}
+ 
+        <div className={styles.topContainer}>
           <div className={styles.rightSection}>
-            <Image
-              src={portfolio2}
-              width={600}
-              height={400}
-              alt="profile image"
-              className={styles.image}
-            />
+            <div className={styles.navigation}></div>
+            <div className={styles.textSection}></div>
           </div>
+          <div className={styles.leftSection}></div>
         </div>
-
-        {/* short bio elevator pitch */}
-        <div className={styles.bioElevatorPitch}>
-          <div className={styles.leftBioSection}>
-            <div className={styles.bio}>
-              <p>
-                With over 20 years of diverse managerial and consulting
-                experience, I am a Digital Transformation Manager with a proven
-                track record in spearheading innovative web and mobile
-                applications, optimizing UX/UI interfaces, and gnerating revenue
-                growth.
-                <br />
-                <br />
-                My multifaceted background in software engineering, event
-                management, customer relations, and team leadership equips me to
-                drive transformative digital changes across various sectors.
-                <br />
-                <br />
-                Passionate about integrating modern technologies with business
-                operations, I&apos;m dedicated to empowering organizations with
-                strategic digital solutions.
-              </p>
-            </div>
-          </div>
-          <div className={styles.rightBioSection}>
-            <div className={styles.redContainer}></div>
-            <div className={styles.bioCTA}>
-              <Link href="/about">More About Me</Link>
-            </div>
-          </div>
-        </div>
+ 
+        <div className={styles.bottomContainer}></div>
 
        
       </div>
@@ -95,3 +36,55 @@ function Where() {
 }
 
 export default Where;
+
+
+<div className={styles.aboutHeader}>
+{/* left section */}
+<div className={styles.leftSection}>
+  <div className={styles.info}>
+    <div className={styles.who}>
+      <FontAwesomeIcon icon={faUserLarge} className={styles.fa} />
+      <span className={styles.w}>W</span>ho
+    </div>
+
+    <div className={styles.what}>
+      <FontAwesomeIcon icon={faCircleInfo} className={styles.fa} />
+      <span className={styles.w}>W</span>hat
+    </div>
+
+    <div className={styles.where}>
+      <FontAwesomeIcon icon={faLocationDot} className={styles.fa} />
+      <span className={styles.w}>W</span> here & Ho
+      <span className={styles.w}>w</span>
+    </div>
+    <div className={styles.when}>
+      <FontAwesomeIcon icon={faCalendarDays} className={styles.fa} />
+      <span className={styles.w}>W</span>hen
+    </div>
+  </div>
+</div>
+
+
+</div>
+
+
+
+
+
+ {/* right section */}
+ <div className={styles.rightSection}>
+ <Image
+   src={portfolio2}
+   width={600}
+   height={400}
+   alt="profile image"
+   className={styles.image}
+ />
+ <Image
+   src={portfolio2}
+   width={900}
+   height={400}
+   alt="profile image"
+   className={styles.image}
+ />
+</div>
