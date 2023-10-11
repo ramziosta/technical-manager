7e81b522bf7d36a2e61b302e030ba06f7dd99209
Ramzi Osta <ramziosta@mac-mini.myfiosgateway.com>
2023-10-11 17:34:29 -0400

    contact form done. next is the footer

--- components/contact/index.jsx
+++ components/contact/index.jsx
@@ -1,115 +1,102 @@
-import  {useState, useRef} from "react";
+import { useState, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./contact.module.scss";
 import emailjs from "@emailjs/browser";
 
-
-
 function Contact() {
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-    const [loading, setLoading] = useState(false);
-  
-  
-    const form = useRef();
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-      const input = document.querySelectorAll("#first_name, #last_name");
-      emailjs.sendForm(
-          "service_eirar1m",
-          "template_lb9v02w",
-          form.current,
-          "wbuyzv3SiIAVgnRc_"
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-            setIsFormSubmitted(true);
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-  
-      //add clear form
-      input.value = "";
-    };
-  
-    return (
-      <>
-        <h2 className="head-text">Contact Me</h2>
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const input = document.querySelectorAll("#first_name, #last_name");
+    emailjs
+      .sendForm(
+        "service_eirar1m",
+        "template_lb9v02w",
+        form.current,
+        "wbuyzv3SiIAVgnRc_"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsFormSubmitted(true);
+          setTimeout(() => {
+            setIsFormSubmitted(false)}, 2500
+          );
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    //add clear form
+    input.value = "";
+  };
+
+  return (
+    <>
+      <div className={styles.email}>
         {!isFormSubmitted ? (
-          // <div className="app__footer-form app__flex">
-             <div className="contact-form"> 
-          <form 
-          ref={form}
-          onSubmit={sendEmail}
-        >
-          <p>
-            <input
-              className="p-text"
-              type="text"
-              required
-              name="user_name"
-              placeholder="Name"
-            />
-          </p>
-          <p>
-            <input
-              className="p-text"
-              type="text"
-              placeholder="Email"
-              required
-              name="user_email"
-            />
-          </p>
-          <p>
-            <input
-              className="p-text"
-              type="text"
-              placeholder="Subject"
-              required
-              name="Subject"
-            />
-          </p>
-          <p>
-            <textarea
-              className="p-text"
-              type="text"
-              placeholder="Message"
-              required
-              name="message"
-            />
-          </p>
-          <p>
-            <button
-              type="submit"
-              value="send"
-              className="send-button"
-            >
-              Send
-            </button>
-          </p>
-        </form>
-     
-        </div>
-          //   <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
-         
-       
+          <>
+            <h2 className={styles.header}>Contact Me</h2>
+            <div>
+              <form ref={form} onSubmit={sendEmail} className={styles.form}>
+              <input
+                className={styles.input}
+                type="text"
+                required
+                name="user_name"
+                placeholder="Name"
+              />
+
+              <input
+                className={styles.input}
+                type="text"
+                placeholder="Email"
+                required
+                name="user_email"
+              />
+
+              <input
+                className={styles.input}
+                type="text"
+                placeholder="Subject"
+                required
+                name="Subject"
+              />
+
+              <textarea
+                className={styles.textarea}
+                type="text"
+                placeholder="Message"
+                required
+                name="message"
+              />
+
+              <button type="submit" value="send" className={styles.button}>
+                Send
+              </button>
+              </form>
+            </div>
+          </>
         ) : (
-          <div>
-            <h3 className="head-text">
+          <div className={styles.confirmation}>
+              <p className={styles.confirmationMessage}>Your email has been sent successfully.</p>
+            <h3 className={styles.confirmationH3}>
               Thank you for getting in touch with me!
             </h3>
+           <p className={styles.confirmationMessage}> I will get back to you as soon as possible.</p> 
           </div>
         )}
-   
+      </div>
     </>
-  );
+);
+
+
 }
 
 export default Contact;
-
-
